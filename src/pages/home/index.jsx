08d4d227526170d1da044e5e420e6808b3da17ae
Willian Giordano Ferreira <williangf@hotmail.com>
2021-06-29 19:00:58 -0300

    Under construction page

---
+++ src/pages/home/index.jsx
@@ -0,0 +1,15 @@
+import { Container } from './styles';
+
+import { IoConstructOutline } from 'react-icons/io5';
+
+function Home() {
+
+  return (
+    <Container>
+      <IoConstructOutline />
+      <h1>Under construction</h1>
+    </Container>
+  )
+}
+
+export default Home;
---
+++ src/pages/home/styles.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+export const Container = styled.main`
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  color: rgb(240, 240, 240);
+
+  svg {
+    font-size: 8rem;
+  }
+
+  h1 {
+    font-size: 2rem;
+  }
+`;
